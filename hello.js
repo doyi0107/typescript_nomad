7111bcf3dd27ac3502455323e04c92ac69bab932
doyi <doyi9917@naver.com>
2023-10-13 21:51:05 +0900

    typescript start

---
+++ hello.js
@@ -0,0 +1,6 @@
+    const doyi = 
+    {name : "doyi" ,
+    hello: function() {
+        console.log('메서드 정의는 이렇게');
+    }}
+doyi.hello();
---
+++ hello.ts
@@ -0,0 +1,6 @@
+let doyi = 
+{name : "doyi" ,
+hello: function() {
+    console.log('메서드 정의는 이렇게');
+  }}
+doyi.hello();
