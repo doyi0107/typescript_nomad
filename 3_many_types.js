a62a6af7ec1a9bc856e1fa0cb95467d40b72bcc6
doyi <doyi9917@naver.com>
2023-10-15 11:17:02 +0900

    types study -ing

--- 3_many_types.js
+++ 3_many_types.js
@@ -1,3 +1,21 @@
+// readonly
 var peoplean = function (name) { return ({ name: name }); };
 var c = peoplean("sss");
 c.age;
+c.name;
+// tuple
+var children = ["yoon", 23, true];
+children[0] = "hi";
+// any
+var v = [1, 2, 3, 4, 5];
+var e = true;
+v + e;
+// unknown
+var d;
+var f = d + 1;
+if (typeof d === 'number') {
+    var f_1 = d + 1;
+}
+function hello() {
+    console.log(xx);
+}
--- 3_many_types.ts
+++ 3_many_types.ts
@@ -1,4 +1,4 @@
-
+// readonly
 
 type people = {
     readonly name:Name,
@@ -9,3 +9,42 @@ const peoplean = (name:string) : people => ({name})
 
 const c = peoplean("sss")
 c.age
+c.name
+
+// tuple
+const children: readonly [string , number, boolean] = ["yoon" , 23, true]
+children[0] = "hi"
+
+
+// any
+const v : any[] = [1,2,3,4,5]
+const e : any = true
+
+v + e 
+
+// unknown
+let d:unknown;
+
+let f = d + 1
+
+if(typeof d === 'number'){
+    let f = d + 1
+}
+
+
+function hello() {
+    console.log(xx);
+}
+
+// never
+function bye(name:string|number) {
+    if(typeof name === "string"){
+          
+    }else if(typeof name === "number"){
+
+    }else{
+        name
+    }
+}
+
+
